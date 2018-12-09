6174f3daf83b4f3966c940ae87b2eb8b85e98712
imtiaz-emu <imtiaz.emu.it@gmail.com>
2018-12-09 12:19:04 +0600

    Deploy config back to Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,45 +1,84 @@
-require 'mina/rails'
-require 'mina/git'
-require 'mina/rvm'
-# require 'mina/puma'
+# config valid only for Capistrano 3.1
+lock '3.2.1'
 
-set :application_name, 'islamjanun'
-set :domain, '184.164.90.50'
-set :deploy_to, '/root/apps/islamjanun'
-set :repository, 'git@github.com:imtiaz-emu/islamjanun.git'
+set :application, 'islamjanun'
+set :repo_url, 'git@github.com:imtiaz-emu/islamjanun.git'
 
-set :shared_dirs, fetch(:shared_dirs, []).push('log', 'tmp/pids', 'tmp/sockets', 'public/uploads')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
-set :user, 'root'
+set :user, 'imtiaz'
+set :use_sudo, false
 
-task :environment do
-  invoke :'rvm:use', 'ruby-2.1.5@default'
-end
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-task :setup do
-  command %[touch "#{fetch(:shared_path)}/config/database.yml"]
-  command %[touch "#{fetch(:shared_path)}/config/secrets.yml"]
-  command %[touch "#{fetch(:shared_path)}/config/puma.rb"]
-  comment "Be sure to edit '#{fetch(:shared_path)}/config/database.yml', 'secrets.yml' and puma.rb."
-end
+# Default deploy_to directory is /var/www/my_app
 
-task :deploy do
-  deploy do
-    comment "Deploying #{fetch(:application_name)} to #{fetch(:domain)}:#{fetch(:deploy_to)}"
-    invoke :'git:clone'
-    invoke :'deploy:link_shared_paths'
-    invoke :'bundle:install'
-    invoke :'rvm:load_env_vars'
-    invoke :'rails:db_migrate'
-    command %{#{fetch(:rails)} db:seed}
-    command 'rake sunspot:solr:stop'
-    command 'rake sunspot:solr:reindex'
-    command 'rake sunspot:solr:start'
-    invoke :'rails:assets_precompile'
-    invoke :'deploy:cleanup'
+# Default value for :scm is :git
+set :scm, :git
 
-    on :launch do
-      invoke :'puma:start'
+# set :deploy_via, :remote_cache # off while it is first deployment
+set :deploy_via, :copy
+
+# Default value for :format is :pretty
+# set :format, :pretty
+set :rvm1_ruby_version, "ruby-2.1.5"
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle}
+set :linked_dirs, fetch(:linked_dirs) + %w{public/system public/uploads}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+set :pty, true
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+
+# set config unicorn.rb, unicorn_init.sh, nginx.conf file permission after deployment
+set :file_permissions_roles, :all
+set :file_permissions_users, ['imtiaz']
+set :file_permissions_chmod_mode, "0777"
+
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 1 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+      # execute "sudo service nginx restart"
+      # execute "sudo service unicorn restart"
+    end
+
+  end
+
+  namespace :solr do
+    task :reindex do
+      run "cd #{current_path} && #{rake} RAILS_ENV=#{rails_env} sunspot:solr:reindex"
+    end
+  end
+
+  before "deploy:updated", "deploy:set_permissions:chmod"
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
     end
   end
 
